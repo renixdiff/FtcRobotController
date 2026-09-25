cb1763155067ccbfbbabb9370209f3d52ce7d278
renixdiff <mohammadabdulhayee222@gmail.com>
2026-09-24 20:56:02 -0400

    Create HelloWorld.java
    created first ever java class.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/HelloWorld.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@Disabled
+@Autonomous
+public class HelloWorld extends OpMode {
+
+    @Override
+    public void init() {
+        telemetry.addData("Hello:","Mohammad"); //My First Comment
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
+
+
